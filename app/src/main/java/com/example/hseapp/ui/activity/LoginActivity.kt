c65b091734a6fc90b111920811ad0259ea34129e
Steve <sparakal@edu.hse.ru>
2022-04-21 20:11:28 +0300

    21-April Commit

--- app/src/main/java/com/example/hseapp/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/example/hseapp/ui/activity/LoginActivity.kt
@@ -2,14 +2,47 @@ package com.example.hseapp.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.example.hseapp.R
 import com.example.hseapp.databinding.ActivityLoginBinding
+import com.example.hseapp.utils.Constants
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
+    private var roleSelected:String = Constants.STUDENT_ROLE
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_login)
+        initView()
+    }
+
+    fun initView() {
+        binding.stud.setOnClickListener {
+            roleSelected = Constants.STUDENT_ROLE
+            setupButton(binding.stud, binding.assist, binding.prof)
+        }
+
+        binding.assist.setOnClickListener {
+            roleSelected = Constants.ASSISTANT_ROLE
+            setupButton(binding.assist, binding.stud, binding.prof)
+        }
+
+        binding.prof.setOnClickListener {
+            roleSelected = Constants.PROFESSOR_ROLE
+            setupButton(binding.prof, binding.assist, binding.stud)
+        }
+    }
+
+    fun setupButton(selectedView:Button, view1:Button, view2:Button) {
+        selectedView.background = getDrawable(R.drawable.role_background)
+        selectedView.setTextColor(getColor(R.color.primary_hse_blue))
+
+        view1.setBackgroundColor(getColor(R.color.white))
+        view1.setTextColor(getColor(R.color.not_selected))
+
+        view2.setBackgroundColor(getColor(R.color.white))
+        view2.setTextColor(getColor(R.color.not_selected))
     }
 }
--- app/src/main/java/com/example/hseapp/utils/Constants.kt
+++ app/src/main/java/com/example/hseapp/utils/Constants.kt
@@ -2,4 +2,10 @@ package com.example.hseapp.utils
 
 object Constants {
     const val SPLASH_DURATION:Long = 2000
+    const val STUDENT_ROLE:String = "student"
+    const val ASSISTANT_ROLE:String = "assistant"
+    const val PROFESSOR_ROLE:String = "professor"
+
+
+
 }
