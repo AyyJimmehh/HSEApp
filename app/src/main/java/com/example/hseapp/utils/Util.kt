cc6aa33969bad974e776744c1596680c49463715
Steve <sparakal@edu.hse.ru>
2022-04-21 20:27:25 +0300

    21-April Commit

---
+++ app/src/main/java/com/example/hseapp/utils/Util.kt
@@ -0,0 +1,11 @@
+package com.example.hseapp.utils
+
+import android.util.Patterns
+
+object Util {
+    fun isValidEmail(email:String?): Boolean {
+        return !email.isNullOrEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    //fun isValidPassword()
+}
