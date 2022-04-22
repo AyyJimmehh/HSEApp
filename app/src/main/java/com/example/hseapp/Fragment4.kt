98e9a4787f5b80541904edb7cab702256003137a
Steve <sparakal@edu.hse.ru>
2022-04-23 02:42:00 +0300

    22 April Commit 2

--- app/src/main/java/com/example/hseapp/Fragment4.kt
+++ app/src/main/java/com/example/hseapp/Fragment4.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class Fragment2 : Fragment() {
+class Fragment4 : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +17,12 @@ class Fragment2 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_2, container, false)
+        return inflater.inflate(R.layout.fragment_4, container, false)
     }
 
     companion object {
         @JvmStatic
         fun newInstance() =
-            Fragment2()
+            Fragment4()
     }
 }
--- app/src/main/java/com/example/hseapp/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/example/hseapp/ui/activity/HomeActivity.kt
@@ -2,7 +2,9 @@ package com.example.hseapp.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.hseapp.R
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.hseapp.*
 import com.example.hseapp.databinding.ActivityHomeBinding
 import com.example.hseapp.databinding.ActivityLoginBinding
 
@@ -10,6 +12,26 @@ class HomeActivity : AppCompatActivity() {
     lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_home)
+        val firstFragment = Fragment1.newInstance()
+        setCurrentFragment(firstFragment)
+        val secondFragment = Fragment2.newInstance()
+        val thirdFragment = Fragment3.newInstance()
+        val fourthFragment = Fragment4.newInstance()
+
+        binding.menubar.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.calender -> setCurrentFragment(firstFragment)
+                R.id.course -> setCurrentFragment(secondFragment)
+                R.id.grade -> setCurrentFragment(thirdFragment)
+                R.id.profile -> setCurrentFragment(fourthFragment)
+            }
+            true
+        }
+
+    }
+
+    fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.homecontainer, fragment, "Menu Item").addToBackStack("Menu Item").commit()
     }
 }
