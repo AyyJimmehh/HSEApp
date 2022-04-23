83d54b935e2c4f915bc764295fd67ac0c673c1e2
Steve <sparakal@edu.hse.ru>
2022-04-23 18:35:43 +0300

    22 April Commit 2

--- app/src/main/java/com/example/hseapp/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/example/hseapp/ui/activity/HomeActivity.kt
@@ -6,29 +6,27 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.hseapp.*
 import com.example.hseapp.databinding.ActivityHomeBinding
-import com.example.hseapp.databinding.ActivityLoginBinding
 
 class HomeActivity : AppCompatActivity() {
     lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_home)
-        val firstFragment = Fragment1.newInstance()
-        setCurrentFragment(firstFragment)
-        val secondFragment = Fragment2.newInstance()
-        val thirdFragment = Fragment3.newInstance()
-        val fourthFragment = Fragment4.newInstance()
+        val calenderFragment = Fragment1.newInstance()
+        setCurrentFragment(calenderFragment)
+        val courseFragment = Fragment2.newInstance()
+        val gradeFragment = Fragment3.newInstance()
+        val userFragment = Fragment4.newInstance()
 
         binding.menubar.setOnItemSelectedListener {
             when(it.itemId) {
-                R.id.calender -> setCurrentFragment(firstFragment)
-                R.id.course -> setCurrentFragment(secondFragment)
-                R.id.grade -> setCurrentFragment(thirdFragment)
-                R.id.profile -> setCurrentFragment(fourthFragment)
+                R.id.calender -> setCurrentFragment(calenderFragment)
+                R.id.course -> setCurrentFragment(courseFragment)
+                R.id.grade -> setCurrentFragment(gradeFragment)
+                R.id.profile -> setCurrentFragment(userFragment)
             }
             true
         }
-
     }
 
     fun setCurrentFragment(fragment: Fragment) {
