6641914db0e46d46bff8a34b3299b339d1c138a5
Steve <sparakal@edu.hse.ru>
2022-06-02 23:24:28 +0300

    02 June Commit

--- app/src/main/java/com/example/hseapp/ui/activity/ChatActivity.kt
+++ app/src/main/java/com/example/hseapp/ui/activity/ChatActivity.kt
@@ -4,19 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.hseapp.R
+import com.example.hseapp.databinding.ActivityChatBinding
 
 class ChatActivity : AppCompatActivity() {
-    lateinit var binding:ChatActivity
+    lateinit var binding:ActivityChatBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        /*binding = DataBindingUtil.setContentView(this,R.layout.activity_chat)
-        binding..backButton.setOnClickListener {
-            onBackPressed()*/
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_chat)
+        binding.chatToolbar.backButton.setOnClickListener {
+            onBackPressed()
         }
     }
 
-    //override fun onBackPressed() {
-        //super.onBackPressed()
-    //}
-//}
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+}
